91d9afa93fce583b45b3661a7de444cf6f4a42c3
caioloureiro <caio.loureiro@hotmail.com>
2025-10-18 14:36:09 -0300

    feat: Atualizar estrutura do menu para melhorar a exibição e usabilidade, incluindo ajustes no filtro e na lógica de exibição de itens

--- admin/view/menu.php
+++ admin/view/menu.php
@@ -3,7 +3,7 @@
 	
 	<div class="menu-titulo">
 		<div class="menu-logo" style="background-image:url(../<?php echo $head_favicon ?>)"></div>
-		<?php echo $head_nome ?>
+		<div class="menu-nome"><?php echo $head_nome ?></div>
 		<div class="menu-voltar"><?php require $raiz_admin .'img/arrow-left.svg'; ?></div>
 	</div>
 	
@@ -13,7 +13,12 @@
 			class="menu-filtro"
 			placeholder="Pesquisar módulo..."
 		/>
-		<div class="menu-filtro-reset"><?php require 'img/fechar.svg'; ?></div>
+		<button type="button" class="menu-filtro-reset<?php echo ($cfg['tema'] === ' escuro' ? ' tema-escuro' : ''); ?>" aria-label="Limpar filtro">
+			<!-- inline SVG com fill=currentColor para poder mudar com CSS -->
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 492 492" width="18" height="18" aria-hidden="true">
+				<path fill="currentColor" d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872 c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872 c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052 L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116 c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952 c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116 c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z"/>
+			</svg>
+		</button>
 	</div>
 	
 	<div class="menu-campo">
@@ -289,53 +294,53 @@ let itens_busca = document.querySelector('.menu-filtro');
 let menu_filtro_reset = document.querySelector('.menu-filtro-reset');
 
 if( itens ){
-	
+
+	function normalizeString( str ){
+		if( !str ) return '';
+		try{
+			return str.normalize('NFD').replace(/\p{Diacritic}/gu, '').toUpperCase();
+		}catch(e){
+			return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase();
+		}
+	}
+
 	itens_busca.addEventListener('keyup', function() {
 
-		let itens_busca = document.querySelector('.menu-filtro').value.toUpperCase();
+		let query = normalizeString( document.querySelector('.menu-filtro').value );
 		let itens = document.querySelector('.menu-campo');
-		
+        
 		let card = itens.querySelectorAll('.menu-btn');
-		
-		menu_filtro_reset.classList.add("on");
-		
-		//console.log( 'card', card );
-		
+        
+		if( query.length > 0 ){
+			menu_filtro_reset.classList.add("on");
+		}else{
+			menu_filtro_reset.classList.remove("on");
+		}
+
 		for( let i = 0; i < card.length; i++ ){
-			
-			//console.log( 'card[i].querySelector(.menu-btn-nome)', card[i].querySelector('.menu-btn-nome') );
 
 			let a = card[i].querySelector('.menu-btn-nome');
-			
-			if( a.innerHTML.toUpperCase().indexOf( itens_busca ) > -1 ){
-				
+			let text = normalizeString( a ? a.textContent : '' );
+            
+			if( text.indexOf( query ) > -1 ){
 				card[i].style.display = '';
-				
 			}else{
-				
 				card[i].style.display = 'none';
-				
 			}
-			
 		}
-		
+        
 	});
-	
-}
-menu_filtro_reset.addEventListener("click", function() {
-	
-	let card = itens.querySelectorAll('.menu-btn');
-	
-	for( let i = 0; i < card.length; i++ ){
 
-		card[i].style.display = '';
-		
-	}
-	
-	itens_busca.value = '';
-	menu_filtro_reset.classList.remove("on");
-	
-});
+	menu_filtro_reset.addEventListener("click", function() {
+		let card = itens.querySelectorAll('.menu-btn');
+		for( let i = 0; i < card.length; i++ ){
+			card[i].style.display = '';
+		}
+		itens_busca.value = '';
+		menu_filtro_reset.classList.remove("on");
+	});
+
+}
 /*End - Filtro REATIVO*/
 
 let topo_btn_menu = document.querySelector('.topo-btn-menu');
